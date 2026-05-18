1d9430c95164c713a6b118e22c079e647ae7056a
aribariyan4-a11y <aribariyan4@gmail.com>
2026-05-18 09:59:47 +0600

    friends grid done and responsive worked

--- src/components/home/FriendsSection.jsx
+++ src/components/home/FriendsSection.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import friends from "@/data/friend.json";
 
-const FriendsSectionPage = () => {
+import FriendCard from "./FriendCard";
+
+export default function FriendsSection() {
+
     return (
-        <div>
-            <h3>this is Friends Section Page</h3>
-        </div>
-    );
-};
 
-export default FriendsSectionPage;
+        <section className="pb-20">
+
+            <div className="w-11/12 max-w-7xl mx-auto">
+
+                {/* Section Heading */}
+                <div className="mb-10">
+
+                    <h2 className="text-4xl font-bold text-gray-900 text-center">
+
+                        Your Friends
+
+                    </h2>
+
+                </div>
+
+                {/* Friends Grid */}
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+
+                    {friends.map((friend) => (
+
+                        <FriendCard
+                            key={friend.id}
+                            friend={friend}
+                        />
+
+                    ))}
+
+                </div>
+
+            </div>
+
+        </section>
+    );
+}
